3446b2e9e3cc57e5c128914b098405fcf4d58212
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-12-20 14:55:44 -0800

    fix: fix a typo

--- lua/llm-sidekick/executables.lua
+++ lua/llm-sidekick/executables.lua
@@ -8,16 +8,16 @@ function M.get_jq_executable()
   return vim.fn.executable('jq') == 1 and 'jq' or error('jq executable not found')
 end
 
-function M.get_sttbuff_executable()
-  if vim.fn.executable('sttbuff') == 1 then
-    return 'sttbuff'
+function M.get_stdbuf_executable()
+  if vim.fn.executable('stdbuf') == 1 then
+    return 'stdbuf'
   end
 
   if vim.fn.has('mac') == 1 and vim.fn.executable('gstdbuf') == 1 then
     return 'gstdbuf'
   end
 
-  error('sttbuff executable not found')
+  error('stdbuf executable not found')
 end
 
 return M
