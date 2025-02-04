fa3aa0bd1e0151b9cdc70a767a6b05741c0da4a3
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2025-02-04 09:44:09 -0800

    fixup! feat: better description for replace_in_file

--- lua/llm-sidekick/tools/file_operations/init.lua
+++ lua/llm-sidekick/tools/file_operations/init.lua
@@ -1,5 +1,5 @@
 return {
   require("llm-sidekick.tools.file_operations.create_or_replace_file"),
-  require("lua.llm-sidekick.tools.file_operations.replace_in_file"),
+  require("llm-sidekick.tools.file_operations.replace_in_file"),
   require("llm-sidekick.tools.file_operations.delete_file"),
 }
