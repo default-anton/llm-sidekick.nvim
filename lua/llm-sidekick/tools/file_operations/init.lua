9fe26a9c11a5553fe2de6b10726763500ffcc500
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2025-02-07 11:25:01 -0800

    chore: remove delete_file

--- lua/llm-sidekick/tools/file_operations/init.lua
+++ lua/llm-sidekick/tools/file_operations/init.lua
@@ -1,5 +1,4 @@
 return {
   require("llm-sidekick.tools.file_operations.write_to_file"),
   require("llm-sidekick.tools.file_operations.search_and_replace_in_file"),
-  -- require("llm-sidekick.tools.file_operations.delete_file"),
 }
