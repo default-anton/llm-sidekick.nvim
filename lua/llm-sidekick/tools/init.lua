c28cf86dcfd4c4bc0b26d676df7f2f80c5d6ab22
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2025-02-11 09:39:28 -0800

    feat: add scratchpad

--- lua/llm-sidekick/tools/init.lua
+++ lua/llm-sidekick/tools/init.lua
@@ -1,9 +1,9 @@
-local file_operations = require("llm-sidekick.tools.file_operations")
+local scratchpad = require("llm-sidekick.tools.scratchpad")
 local communication = require("llm-sidekick.tools.communication")
+local file_operations = require("llm-sidekick.tools.file_operations")
 
-local tools = {}
-
-vim.list_extend(tools, file_operations)
+local tools = { scratchpad }
 vim.list_extend(tools, communication)
+vim.list_extend(tools, file_operations)
 
 return tools
